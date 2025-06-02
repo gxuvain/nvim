68a40a96534bd74705ccbbe9d9c128f46fe81fc5
Gauvain Palanga <gauvainpalanga@gmail.com>
2025-06-02 19:04:30 +0200

    feat: add new plugins and remove unused ones

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,13 +2,13 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = ":TSUpdate",
     config = function()
-	local configs = require("nvim-treesitter.configs")
-	configs.setup({
-	    highlight = { enable = true },
-	    ident = { enable = true },
-	    autotage = { enable = true },
-	    ensure_installed = { "lua" },
-	    auto_install = false
-	})
+        local configs = require("nvim-treesitter.configs")
+        configs.setup({
+            highlight = { enable = true },
+            ident = { enable = true },
+            autotage = { enable = true },
+            ensure_installed = { "lua" },
+            auto_install = false
+        })
     end
 }
